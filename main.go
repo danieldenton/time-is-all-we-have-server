8518e54a5faaf2052271d02c0a46aa892e5be656
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-09-30 16:17:48 -0700

    post request working

--- main.go
+++ main.go
@@ -24,8 +24,20 @@ func getTasks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, tasks)
 }
 
+func createTask(c *gin.Context) {
+	var newTask task
+
+	if err := c.BindJSON(&newTask); err != nil {
+		return
+	}
+
+	tasks = append(tasks, newTask)
+	c.IndentedJSON(http.StatusCreated, newTask)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/tasks", getTasks)
+	router.POST("/tasks", createTask)
 	router.Run("localhost:8080")
 }
