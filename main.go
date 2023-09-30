362476feaa1e99f6c4ada8892ef1b33bdb965e76
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-09-30 16:06:00 -0700

    id set to int

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package server
+package main
 
 import (
 	"net/http"
@@ -7,17 +7,17 @@ import (
 )
 
 type task struct {
-	ID      string `json: "id"`
+	ID      int    `json: "id"`
 	Name    string `json: "task"`
 	Time    string `json: "time"`
 	Minutes int    `json: "minutes"`
 }
 
 var tasks = []task{
-	{ID: "1", Name: "meditaion", Time: "10:00 am", Minutes: 30},
-	{ID: "2", Name: "bjj", Time: "11:45 am", Minutes: 120},
-	{ID: "3", Name: "work", Time: "2:00 pm", Minutes: 240},
-	{ID: "4", Name: "study", Time: "7:00 pm", Minutes: 90},
+	{ID: 1, Name: "meditaion", Time: "10:00 am", Minutes: 30},
+	{ID: 2, Name: "bjj", Time: "11:45 am", Minutes: 120},
+	{ID: 3, Name: "work", Time: "2:00 pm", Minutes: 240},
+	{ID: 4, Name: "study", Time: "7:00 pm", Minutes: 90},
 }
 
 func getTasks(c *gin.Context) {
